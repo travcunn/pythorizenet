8ca943eef62c0b241864d6231971e18a62d8375b
btimby <btimby@40e8dbea-03a0-11de-9a74-2f65630b6d95>
2009-02-26 16:11:28 +0000

    - updated to include parsing of response.

--- src/arb.py
+++ src/arb.py
@@ -1,23 +1,36 @@
 import httplib, urllib, datetime
 from lxml import etree
 
-UNIT_MONTH = 'month'
+UNIT_MONTH = 'months'
 UNIT_DAYS = 'days'
 
 TYPE_CREDIT = 'credit'
 
 PERIOD_ONGOING = 9999
 
+HOST_PROD = 'api.authorize.net'
+HOST_TEST = 'apitest.authorize.net'
+PATH = '/xml/v1/request.api'
+
 class AuthorizeNet(object):
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, host):
+        self.host = host
 
     def send(self, data):
-        pass
+        #data = urllib.quote(data)
+        conn = httplib.HTTPSConnection(self.host)
+        conn.putrequest('POST', PATH)
+        conn.putheader('content-type', 'text/xml')
+        conn.putheader('content-length', len(data))
+        conn.endheaders()
+        conn.send(data)
+
+        response = conn.getresponse()
+        return response.read()
 
 class Recurring(object):
-    def __init__(self, url, login, key):
-        connection = AuthorizeNet(url)
+    def __init__(self, host, login, key):
+        self.conn = AuthorizeNet(host)
         self.login = login
         self.key = key
         self.schedule = None
@@ -76,20 +89,20 @@ class Recurring(object):
             etree.SubElement(schedule, 'totalOccurrences').text = str(total)
             etree.SubElement(schedule, 'trialOccurrences').text = str(trial)
         if self.amount:
-            etree.SubElement(root, 'amount').text = str(self.amount)
+            etree.SubElement(subscription, 'amount').text = str(self.amount)
         if self.trialAmount:
-            etree.SubElement(root, 'trialAmount').text = str(self.trialAmount)
+            etree.SubElement(subscription, 'trialAmount').text = str(self.trialAmount)
         if self.payment:
             type = self.payment[0]
             if type == TYPE_CREDIT:
                 (card_num, card_exp) = self.payment[1]
-                payment = etree.SubElement(root, 'payment')
+                payment = etree.SubElement(subscription, 'payment')
                 credit = etree.SubElement(payment, 'creditCard')
                 etree.SubElement(credit, 'cardNumber').text = card_num
-                etree.SubElement(credit, 'cardExpiration').text = '%s-%s' % card_exp
+                etree.SubElement(credit, 'expirationDate').text = '%s-%s' % card_exp
         if self.customer:
             (first_name, last_name, company, address, city, state, zip, country) = self.customer
-            customer = etree.SubElement(root, 'billTo')
+            customer = etree.SubElement(subscription, 'billTo')
             etree.SubElement(customer, 'firstName').text = first_name
             etree.SubElement(customer, 'lastName').text = last_name
             if company:
@@ -105,31 +118,42 @@ class Recurring(object):
             if country:
                 etree.SubElement(customer, 'country').text = country
         if self.subscription_id:
-            etree.SubElement(root, 'subscriptionId').text = self.subscription_id
+            etree.SubElement(subscription, 'subscriptionId').text = self.subscription_id
         return etree.tostring(root, xml_declaration=True, encoding='utf-8')
 
     def _fromXml(self, response):
-        pass
+        #TODO: investigate why etree will not parse this document when this namespace definition is intact.
+        # for now just remove it :-)
+        response = response.replace(' xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd"', '')
+        root = etree.XML(response)
+
+        messages = root.find('messages')
+        if messages.find('resultCode').text == 'Error':
+            message = messages.find('message')
+            raise Exception('%s - %s' % (message.find('code').text, message.find('text').text))
 
     def create(self):
         xml = self._toXml('ARBCreateSubscriptionRequest')
-        response = self.connnection.send(xml)
+        response = self.conn.send(xml)
         self.subscription_id = self._fromXml(response)
 
     def update(self):
         xml = self._toXml('ARBUpdateSubscriptionRequest')
-        response = self.connnection.send(xml)
+        response = self.conn.send(xml)
         self._fromXml(response)
 
     def cancel(self):
         xml = self._toXml('ARBCancelSubscriptionRequest')
-        response = self.connnection.send(xml)
+        response = self.conn.send(xml)
         self._fromXml(response)
 
 if __name__ == '__main__':
-    test = Recurring('https://blah.com/', 'test', 'test')
+    import sys
+    test = Recurring(HOST_TEST, sys.argv[1], sys.argv[2])
     test.add_schedule()
     test.add_amount('10.00')
     test.add_credit('1111111111111111', ('2010', '03'))
     test.add_customer('ben', 'timby')
+    import pdb; pdb.set_trace()
+    test.create()
     print test._toXml('ARBCreateSubscriptionRequest')
