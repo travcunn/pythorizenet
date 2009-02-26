9993ac6b415ed906dd92958f3d727ad904b48339
btimby <btimby@40e8dbea-03a0-11de-9a74-2f65630b6d95>
2009-02-26 18:32:17 +0000

    - response now successfully parsed. Exception raised with error details.

--- trunk/src/arb.py
+++ trunk/src/arb.py
@@ -35,26 +35,25 @@ class Recurring(object):
         self.key = key
         self.schedule = None
         self.amount = None
-        self.trialAmount = None
+        self.trial = None
         self.payment = None
         self.customer = None
         self.subscription_id = None
 
-    def add_schedule(self, period=PERIOD_ONGOING, start=None, count=1, trial=0, unit=UNIT_MONTH):
+    def add_schedule(self, period=PERIOD_ONGOING, start=None, count=1, unit=UNIT_MONTH):
         if not start:
             start = datetime.datetime.now()
-        total = period + trial
-        self.schedule = (start, total, count, trial, unit)
+        self.schedule = (start, period, count, unit)
 
     def add_amount(self, amount):
         if not isinstance(amount, str):
             raise Exception('You must provide the amount as a string!')
         self.amount = amount
 
-    def add_trialAmount(self, trialAmount):
+    def add_trial(self, trialPeriod, trialAmount):
         if not isinstance(trialAmount, str):
             raise Exception('You must provide the trial amount as a string!')
-        self.trialAmount = trialAmount
+        self.trial = (trialPeriod, trialAmount)
 
     def add_credit(self, card_num, card_exp):
         if not isinstance(card_exp, (tuple, list)):
@@ -80,18 +79,20 @@ class Recurring(object):
         etree.SubElement(auth, "transactionKey").text = self.key
         subscription = etree.SubElement(root, 'subscription')
         if self.schedule:
-            (start, total, count, trial, unit) = self.schedule
+            (start, total, count, unit) = self.schedule
             schedule = etree.SubElement(subscription, 'paymentSchedule')
             interval = etree.SubElement(schedule, 'interval')
             etree.SubElement(interval, 'length').text = str(count)
             etree.SubElement(interval, 'unit').text = str(unit)
             etree.SubElement(schedule, 'startDate').text = start.strftime('%Y-%m-%d')
+            if self.trial:
+                total += self.trial[0]
+                etree.SubElement(schedule, 'trialOccurrences').text = str(self.trial[0])
             etree.SubElement(schedule, 'totalOccurrences').text = str(total)
-            etree.SubElement(schedule, 'trialOccurrences').text = str(trial)
         if self.amount:
             etree.SubElement(subscription, 'amount').text = str(self.amount)
-        if self.trialAmount:
-            etree.SubElement(subscription, 'trialAmount').text = str(self.trialAmount)
+        if self.trial:
+            etree.SubElement(subscription, 'trialAmount').text = str(self.trial[1])
         if self.payment:
             type = self.payment[0]
             if type == TYPE_CREDIT:
@@ -128,9 +129,12 @@ class Recurring(object):
         root = etree.XML(response)
 
         messages = root.find('messages')
-        if messages.find('resultCode').text == 'Error':
+        result_code = messages.find('resultCode').text
+        if result_code == 'Error':
             message = messages.find('message')
             raise Exception('%s - %s' % (message.find('code').text, message.find('text').text))
+        if result_code == 'Ok':
+            return root.find('subscriptionId').text
 
     def create(self):
         xml = self._toXml('ARBCreateSubscriptionRequest')
@@ -153,6 +157,7 @@ if __name__ == '__main__':
     test = Recurring(HOST_PROD, sys.argv[1], sys.argv[2])
     test.add_schedule()
     test.add_amount('10.00')
-    test.add_credit('1111111111111111', ('2010', '03'))
-    test.add_customer('ben', 'timby')
+    test.add_credit('4427802718148774', ('2010', '03'))
+    test.add_customer('john', 'smith')
     test.create()
+    print test.subscription_id
