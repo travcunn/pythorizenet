40690807eafa8b66749ae03979208be7695dffcb
btimby <btimby@40e8dbea-03a0-11de-9a74-2f65630b6d95>
2009-12-01 22:42:43 +0000

    - moved to setuptools instead of distutils.     * done to allow install_requires dependency on lxml  - now using hashlib instead of md5 (with failback).  - now raises an ImportError when lxml is not installed.

--- src/pythorizenet/__init__.py
+++ src/pythorizenet/__init__.py
@@ -1,7 +1,17 @@
-import md5
+try:
+    import lxml
+except ImportError:
+    raise ImportError('pythorizenet reqires lxml')
+
+try:
+    import hashlib
+    make_md5 = hashlib.md5
+except ImportError:
+    import md5
+    make_md5 = md5.new
 
 def generate_hash(*args):
     """generate_hash(hash_key, trans_id, amount)"""
-    m = md5.new()
+    m = make_md5()
     map(m.update, args)
     return m.digest().encode('hex').upper()
