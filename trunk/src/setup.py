8dabfa9c7a290a914a714a914b91c5befd0ffda5
btimby <btimby@40e8dbea-03a0-11de-9a74-2f65630b6d95>
2009-12-01 22:42:43 +0000

    - moved to setuptools instead of distutils.     * done to allow install_requires dependency on lxml  - now using hashlib instead of md5 (with failback).  - now raises an ImportError when lxml is not installed.

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -1,25 +1,27 @@
 #! /usr/bin/env python
 
 import os, string, sys
-from distutils.core import setup
+from setuptools import setup
 
 PACKAGE = "pythorizenet"
 
 def main():
-    setup(
-        name                    = "%s" % PACKAGE,
-        version                 = "1.0",
-        description             = "Python classes for the Authorize.net AIM and ARB APIs.",
-        author                  = "Ben Timby",
-        author_email            = "btimby@ftphosting.net",
-        maintainer              = "Ben Timby",
-        maintainer_email        = "btimby@ftphosting.net",
-        url                     = "http://www.ftphosting.net/",
-        license                 = "GPL",
-        platforms               = "UNIX",
-        long_description        = "AIM and ARB API interfaces for performing real-time credit card authorizations/captures as well as automated recurring billing.",
-        packages                = ['pythorizenet']
-    )
+    kwargs = {
+        'name'                    : "%s" % PACKAGE,
+        'version'                 : "1.0",
+        'description'             : "Python classes for the Authorize.net AIM and ARB APIs.",
+        'author'                  : "Ben Timby",
+        'author_email'            : "btimby@ftphosting.net",
+        'maintainer'              : "Ben Timby",
+        'maintainer_email'        : "btimby@ftphosting.net",
+        'url'                     : "http://www.ftphosting.net/",
+        'license'                 : "GPL",
+        'platforms'               : "UNIX",
+        'long_description'        : "AIM and ARB API interfaces for performing real-time credit card authorizations/captures as well as automated recurring billing.",
+        'packages'                : ['pythorizenet'],
+        'install_requires'        : ['python-lxml >= 1.3.4'],
+    }
+    setup(**kwargs)
 
 if __name__ == "__main__":
 	main()
