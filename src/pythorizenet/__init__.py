0e19b8469512e20543ae9a8494713b132d359afb
btimby <btimby@40e8dbea-03a0-11de-9a74-2f65630b6d95>
2010-03-30 20:58:17 +0000

    - fixes to use hashlib when available, simpler method.

--- src/pythorizenet/__init__.py
+++ src/pythorizenet/__init__.py
@@ -1,14 +1,12 @@
 import lxml
 
 try:
-    import hashlib
-    make_md5 = hashlib.md5
+    from hashlib import md5
 except ImportError:
-    import md5
-    make_md5 = md5.new
+    from md5 import new as md5
 
 def generate_hash(*args):
     """generate_hash(hash_key, trans_id, amount)"""
-    m = make_md5()
+    m = md5()
     map(m.update, args)
-    return m.digest().encode('hex').upper()
+    return m.hexdigest().upper()
